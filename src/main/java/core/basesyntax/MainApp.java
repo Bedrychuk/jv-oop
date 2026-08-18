9c2b0bf17e143311750acaed1affa3c6cead13fc
Danylo Bedrychuk <bedricukdanilo@gmail.com>
2026-08-18 23:29:32 +0300

    Implemented Machine architecture

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,9 +2,9 @@ package core.basesyntax;
 
 public class MainApp {
     public static void main(String[] args) {
-        Truck truck = new Truck();
-        Bulldozer bulldozer = new Bulldozer();
-        Excavator excavator = new Excavator();
+        Machine truck = new Truck();
+        Machine bulldozer = new Bulldozer();
+        Machine excavator = new Excavator();
         Machine[] cars = {truck, bulldozer, excavator};
         for (int i = 0; i < cars.length; i++) {
             cars[i].doWork();
